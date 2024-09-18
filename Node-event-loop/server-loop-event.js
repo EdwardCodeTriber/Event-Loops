1604d669a2ad04f87eed791df7ca4c07cd0f4fe0
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-18 18:17:40 +0200

    Imported readlineSync

---
+++ Node-event-loop/server-loop-event.js
@@ -0,0 +1,48 @@
+const readlineSync = require("readline-sync");
+
+// Questions
+const questions = [
+  { question: "What is 2 + 2?", answer: "4", timeLimit: 10 },
+  {
+    question: "What is the capital of France?",
+    answer: "Paris",
+    timeLimit: 10,
+  },
+  { question: "What is the color of the sky?", answer: "blue", timeLimit: 10 },
+  {
+    question: "What is the Best car in the world?",
+    answer: "BMW",
+    timeLimit: 10,
+  },
+  {
+    question: "What is the full name for this accronim www?",
+    answer: "world wide web",
+    timeLimit: 10,
+  },
+  {
+    question:
+      "Which world statesman and national hero was formerly known as Prisoner 46664?",
+    answer: "Nelson Mandela",
+    timeLimit: 10,
+  },
+  {
+    question: "In what country is the Mercedes Benz company based?",
+    answer: "germany",
+    timeLimit: 10,
+  },
+  {
+    question: "What is the longest line of constant latitude of the Earth?",
+    answer: "Equator",
+    timeLimit: 10,
+  },
+  {
+    question: "The symbol for which car manufacturer is four linked circles?",
+    answer: "Audi",
+    timeLimit: 10,
+  },
+  {
+    question: "Who was the favourite president in RSA?",
+    answer: "Zuma",
+    timeLimit: 10,
+  },
+];
